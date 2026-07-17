71bd116643d172e3051dbc1dbd03ef1966c1fa4f
Minkin Aleksei <thespiritofnetwork@gmail.com>
2026-07-17 13:08:52 +0200

    style: rustfmt readme example

--- crates/yt-rs/examples/readme.rs
+++ crates/yt-rs/examples/readme.rs
@@ -6,12 +6,9 @@ async fn main() {
     let token = std::env::var("YOUTRACK_TOKEN").expect("set YOUTRACK_TOKEN env variable");
 
     let http = reqwest::Client::new();
-    let client = YoutrackClient::new(
-        http,
-        host.as_str(),
-        AuthorizationFlow::PermanentBearerToken(token),
-    )
-    .unwrap();
+    let client =
+        YoutrackClient::new(http, host.as_str(), AuthorizationFlow::PermanentBearerToken(token))
+            .unwrap();
     let users_api = client.users_api();
 
     let fields_query = vec!["login".into(), "id".into()].into();
